ff87e731d4ccadf5cf68363f88815b22f006082f
BrunoZambuze <zambuzesilva@hotmail.com>
2024-11-07 07:41:03 -0300

    Desambiguação de beans utilizando o Primary e Qualifier

--- algafood-api/src/main/java/com/algaworks/algafood_api/notificacao/NotificadorSMS.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/notificacao/NotificadorSMS.java
@@ -4,14 +4,14 @@ import com.algaworks.algafood_api.model.Cliente;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Qualifier("email")
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorSMS implements Notificador {
 
+    @Qualifier("sms")
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        System.out.printf("%s foi enviado uma mensagem para o e-mail '%s': %s",
-                          cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("%s foi enviado uma mensagem para o telefone '%s': %s",
+                          cliente.getNome(), cliente.getTelefone(), mensagem);
     }
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood_api/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/service/AtivacaoClienteService.java
@@ -2,14 +2,21 @@ package com.algaworks.algafood_api.service;
 
 import com.algaworks.algafood_api.model.Cliente;
 import com.algaworks.algafood_api.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
+    @Qualifier("email")
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador){
-        this.notificador = notificador;
-    }
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador){
+//        this.notificador = notificador;
+//    }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
