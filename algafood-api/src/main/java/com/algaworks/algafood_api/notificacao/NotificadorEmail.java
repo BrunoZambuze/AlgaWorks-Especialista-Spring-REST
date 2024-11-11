4fcf4279a3cf36c50c69db3334723171deeb6a5a
BrunoZambuze <zambuzesilva@hotmail.com>
2024-11-11 06:50:25 -0300

    Criando propriedades personalizadas com @ConfigurationProperties

--- algafood-api/src/main/java/com/algaworks/algafood_api/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/notificacao/NotificadorEmail.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood_api.notificacao;
 
 import com.algaworks.algafood_api.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Autowired
+    private NotificadorProperties properties;
+
     @Override
     public void notificar(Cliente cliente, String mensagem){
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
+
         System.out.printf("%s foi enviado uma mensagem para o e-mail '%s': %s",
                           cliente.getNome(), cliente.getEmail(), mensagem);
     }
---
+++ algafood-api/src/main/java/com/algaworks/algafood_api/notificacao/NotificadorProperties.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood_api.notificacao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email") //Estamos dizendo ao Spring que essa classe representa um arquivo de configurações de propriedades
+public class NotificadorProperties {
+
+    /**
+     * Host do servidor de e-mail
+     */
+    private String hostServidor;
+
+    /**
+     * Porta do servidor de e-mail
+     */
+    private Integer portaServidor;
+
+    public String getHostServidor(){
+        return this.hostServidor;
+    }
+
+    public void setHostServidor(String hostServidor){
+        this.hostServidor = hostServidor;
+    }
+
+    public Integer getPortaServidor(){
+        return this.portaServidor;
+    }
+
+    public void setPortaServidor(Integer portaServidor){
+        this.portaServidor = portaServidor;
+    }
+
+}
