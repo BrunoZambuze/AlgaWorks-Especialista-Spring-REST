4494b103d789c273bc94ac1dfee96a58f4e4a5f7
BrunoZambuze <zambuzesilva@hotmail.com>
2024-11-18 07:38:57 -0300

    Crud utilizando somente o JPA

---
+++ algafood-api/src/main/java/com/algaworks/algafood_api/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood_api.jpa;
+
+import com.algaworks.algafood_api.AlgafoodApiApplication;
+import com.algaworks.algafood_api.domain.model.Cozinha;
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+        for(Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+    }
+
+}
---
+++ algafood-api/src/main/java/com/algaworks/algafood_api/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood_api.jpa;
+
+import com.algaworks.algafood_api.AlgafoodApiApplication;
+import com.algaworks.algafood_api.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Mexicana");
+
+        cadastroCozinha.adicionarCozinha(cozinha1);
+        cadastroCozinha.adicionarCozinha(cozinha2);
+        }
+    }
