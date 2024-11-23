a852252592aaef07e92b46af15eebe02ed41609a
BrunoZambuze <zambuzesilva@hotmail.com>
2024-11-23 14:48:38 -0300

    Adicionado o Content Negotiation de teste, buscar cozinha pelo id e customizar a representação do recurso

--- algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
@@ -4,24 +4,29 @@ package com.algaworks.algafood_api.api.controller;
 import com.algaworks.algafood_api.domain.model.Cozinha;
 import com.algaworks.algafood_api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
     }
 
-
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.buscar(cozinhaId);
+    }
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood_api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+//@JsonRootName(value = "gastronomia")
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -15,6 +19,8 @@ public class Cozinha {
     @EqualsAndHashCode.Include
     private Long id;
 
+//    @JsonIgnore
+    @JsonProperty(value = "titulo")
     @Column(length = 30, nullable = false)
     private String nome;
 
