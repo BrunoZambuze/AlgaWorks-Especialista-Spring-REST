fcf3ca8df97701b858c5f0d92bcb1ee0bc3bcd5c
BrunoZambuze <zambuzesilva@hotmail.com>
2025-01-13 14:58:52 -0300

    Estendendo o Problem para adicionar propriedades com constraints violadas graças ao @BeanValidation

--- algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -86,7 +87,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ProblemType problemType = ProblemType.DADOS_INVALIDOS;
         String detail = "Um ou mais campos estão preenchidos de maneira errada. Por favor corrija-os";
-        Problem problem = createProblemBuilder(status, problemType, detail).uiMessage(MSG_ERRO_GENERICA_USUARIO_FINAL).build();
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<Problem.Field> problemFields = bindingResult.getFieldErrors()
+                .stream()
+                .map(fieldError -> Problem.Field.builder()
+                        .name(fieldError.getField())
+                        .uiMessage(fieldError.getDefaultMessage())
+                        .build())
+                .collect(Collectors.toList());
+
+        Problem problem = createProblemBuilder(status, problemType, detail)
+                .uiMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
+                .fields(problemFields)
+                .build();
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
 
--- algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionhandler/Problem.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionhandler/Problem.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL) //Incluir na representação as propriedades que não estiverem nulas
 @Getter
@@ -18,4 +19,13 @@ public class Problem {
 
     private String uiMessage;
     private LocalDateTime timestamp;
+
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field{
+        private String name;
+        private String uiMessage;
+    }
 }
